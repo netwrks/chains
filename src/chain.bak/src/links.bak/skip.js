83a690cccdcabbd9f493ed1440aa25c8e4f191d7
jzierau <jeremy@jzierau.com>
2022-02-16 20:11:46 -0500

    merge base utils into index

--- src/chain.bak/src/links.bak/skip.js
+++
@@ -1,8 +0,0 @@
-module.exports = r => d => {
-  let _d = {}, config = { label: 'SKIP' }, t = 0;
-  const { guide } = require('../utils');
-  const D = { ..._d, ...d };
-  guide([`[${config.label}]`,2,D]);
-  t = 0;
-  return r(D);
-};
